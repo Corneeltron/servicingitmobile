20c8c79c04b2a290cf91e66ecc51199ef07a21d7
corneliusphanthanh <cornelius.phanthanh@pnmac.com>
2023-03-12 15:21:48 +0900

    Validation

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -15,58 +15,59 @@ import {TextInput, Button, Headline, Text} from 'react-native-paper';
 import {useDispatch, useSelector} from 'react-redux';
 import {setCredentials} from '../../redux/slices/authSlice';
 import {useLoginMutation} from '../../redux/slices/authApiSlice';
+import {Formik} from 'formik';
+import {loginForm} from './LoginFormValidation'
 
 export const Login = () => {
-  const userRef = useRef();
-  const errRef = useRef();
-  const [user, setUser] = useState('');
-  const [pwd, setPwd] = useState('');
-  const [errMsg, setErrMsg] = useState('');
+  // const userRef = useRef();
+  // const errRef = useRef();
+  // const [email, setEmail] = useState('');
+  // const [pwd, setPwd] = useState('');
+  // const [errMsg, setErrMsg] = useState('');
   const navigation = useNavigation();
-  const [login, {isLoading}] = useLoginMutation();
-  const dispatch = useDispatch();
+  // const [login, {isLoading}] = useLoginMutation();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    setErrMsg('');
-  }, [user, pwd]);
+  // useEffect(() => {
+  //   setErrMsg('');
+  // }, [email, pwd]);
 
-  const handleSubmit = async e => {
-    // e.preventDefault();
-    try {
-      const userData = await login({user, pwd}).unwrap(); // unwrap response here to get the token that we send off in dispatch on 43
-      dispatch(setCredentials({...userData, user}));
-      setUser('');
-      setPwd('');
-      navigation.navigate('Dashboard');
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg('No Server Response');
-      } else if (err.response?.status === 400) {
-        setErrMsg('Missing Username or Password');
-      } else if (err.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      } else {
-        setErrMsg('Login Failed');
-      }
-    }
-  };
+  // const handleSubmit = async e => {
+  //   // e.preventDefault();
+  //   try {
+  //     const userData = await login({user, pwd}).unwrap(); // unwrap response here to get the token that we send off in dispatch on 43
+  //     dispatch(setCredentials({...userData, user}));
+  //     setUser('');
+  //     setPwd('');
+  //     navigation.navigate('Dashboard');
+  //   } catch (err) {
+  //     if (!err?.response) {
+  //       setErrMsg('No Server Response');
+  //     } else if (err.response?.status === 400) {
+  //       setErrMsg('Missing Username or Password');
+  //     } else if (err.response?.status === 401) {
+  //       setErrMsg('Unauthorized');
+  //     } else {
+  //       setErrMsg('Login Failed');
+  //     }
+  //   }
+  // };
 
   const navigateToRegister = () => {
     navigation.navigate('Register');
   };
 
-  const content = 
-  // isLoading ? (
-  //   <ActivityIndicator />
-  // ) : 
-  (
-    <SafeAreaView
-      style={styles.container}
-      testID="search-page">
+  const navigateToHome = () => navigation.navigate('Calendar')
+
+  const content = (
+    // isLoading ? (
+    //   <ActivityIndicator />
+    // ) :
+    <SafeAreaView style={styles.container} testID="search-page">
       <View style={styles.loginContainer}>
-        <Text ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
+        {/* <Text ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
           {errMsg}
-        </Text>
+        </Text> */}
         <View
           style={{
             justifyContent: 'center',
@@ -81,44 +82,56 @@ export const Login = () => {
           </Headline>
         </View>
         <View>
-          <TextInput
-            mode="outlined"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            testID="username-input"
-            style={{marginTop: 10}}
-            label="E-mail"
-            // onBlur={onBlur}
-            onChangeText={user => setUser(user)}
-            value={user}
-          />
-          <TextInput
-            secureTextEntry={true}
-            mode="outlined"
-            testID="password-input"
-            style={{marginTop: 10}}
-            label="Password"
-            // onBlur={onBlur}
-            onChangeText={password => setPwd(password)}
-            value={pwd}
-          />
-          <Button style={styles.button} uppercase={false}>Forgot email/password</Button>
-          <Button
-            testID="login-button"
-            style={{...styles.button, marginTop: 10}}
-            icon="account"
-            mode="contained"
-            onPress={() => handleSubmit()}>
-            Log in
-          </Button>
-          <Button
-            testID="register-button"
-            style={styles.button}
-            icon="lock"
-            mode="outlined"
-            onPress={() => navigateToRegister()}>
-            Sign up
-          </Button>
+          <Formik initialValues={{email: '', password: ''}} onSubmit={navigateToHome} validationSchema={loginForm}>
+            {({handleSubmit, handleChange, setFieldTouched, touched, values, errors}) => (
+              <>
+                <TextInput
+                  mode="outlined"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  testID="email-input"
+                  style={{marginTop: 10}}
+                  label="E-mail"
+                  onChangeText={handleChange('email')}
+                  onFocus={() => setFieldTouched('email')}
+                />
+                {
+                  touched.email && errors.email ? <Text style={styles.error}>{errors.email}</Text> : null
+                }
+                <TextInput
+                  secureTextEntry={true}
+                  mode="outlined"
+                  testID="password-input"
+                  style={{marginTop: 10}}
+                  label="Password"
+                  onChangeText={handleChange('password')}
+                  onFocus={() => setFieldTouched('password')}
+                />
+                {
+                  touched.password && errors.password ? <Text style={styles.error}>{errors.password}</Text> : null
+                }
+                <Button disabled={!values.email || errors.email} style={styles.button} uppercase={false}>
+                  Forgot email/password
+                </Button>
+                <Button
+                  testID="login-button"
+                  style={{...styles.button, marginTop: 10}}
+                  icon="account"
+                  mode="contained"
+                  onPress={handleSubmit}>
+                  Log in
+                </Button>
+                <Button
+                  testID="register-button"
+                  style={styles.button}
+                  icon="lock"
+                  mode="outlined"
+                  onPress={navigateToRegister}>
+                  Sign up
+                </Button>
+              </>
+            )}
+          </Formik>
         </View>
       </View>
     </SafeAreaView>
@@ -129,16 +142,20 @@ export const Login = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
+    display: 'flex',
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   loginContainer: {
-    width: "80%"
+    width: '80%',
   },
   button: {
     marginTop: 20,
     marginHorizontal: 10,
   },
+  error: {
+    color: "red",
+    marginTop: 3
+  }
 });
--- src/screens/Login/__tests__/Login.test.js
+++ src/screens/Login/__tests__/Login.test.js
@@ -1,10 +1,12 @@
 import {Login} from '../Login';
 import {render} from '@testing-library/react-native';
 
-describe('Login screen', () => {
-  it('should go to home page on login', () => {
-    const page = render(<Login />);
+// describe('Login screen', () => {
+//   it('should go to home page on login', () => {
+//     const page = render(<Login />);
 
-    const loginButton = page.getByTestId('loginButton')
-  })
-})
+//     const loginButton = page.getByTestId('loginButton')
+//     fireEvent.press(loginButton);
+//     expect(navigation.navigate).toHaveBeenCalledWith("Calendar");
+//   })
+// })
