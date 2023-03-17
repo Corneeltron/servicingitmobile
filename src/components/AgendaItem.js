579c304db7b2969e13fa69ba139688b4af852261
corneliusphanthanh <cornelius.phanthanh@pnmac.com>
2023-03-17 10:44:12 +0900

    Changing to slices

--- src/components/AgendaItem.js
+++ src/components/AgendaItem.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import {Text, Card, Avatar} from 'react-native-paper';
 
 export default function AgendaItem(props) {
@@ -27,7 +27,7 @@ export default function AgendaItem(props) {
       <TouchableOpacity
         onPress={() => setSelected(!selected)}
         style={styles.itemTextWrapper}>
-        <Card mode="contained" style={styles.shadowProps}>
+        <Card mode="contained">
           <Card.Content>
             <Card.Title
               titleVariant="titleLarge"
@@ -38,28 +38,12 @@ export default function AgendaItem(props) {
                 <Avatar.Text {...props} label={`${avatarLetter}`} />
               )}
             />
-            <Text style={selected ? styles.contentShow : styles.content} variant="bodySmall">{props.item.value}</Text>
+              <Text>Customer Information</Text>
+            <View style={styles.customerInfo} variant="bodySmall">
+              </View>
           </Card.Content>
         </Card>
       </TouchableOpacity>
-      {/* <div className={styles.wrapper}>
-        <div className={styles.accordian}>
-          {data.map((item, idx) => (
-            <div key={idx} className={styles.item} onClick={() => toggle(idx)}>
-              <div className={styles.title}>
-                <h2>{item.question}</h2>
-                <span>{selected === idx ? '-' : '+'}</span>
-              </div>
-              <div
-                className={
-                  selected === idx ? styles.contentShow : styles.content
-                }>
-                {item.value}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div> */}
     </>
   );
 }
@@ -69,42 +53,20 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginTop: 17,
   },
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    height: '75vh',
-    width: '75vw',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  accordian: {
-    width: 500,
-    cursor: 'pointer',
-  },
-
-  item: {
-    background: '#f0ebe1',
-    borderRadius: 10,
-    marginBottom: 5,
-    padding: '10px 20px',
-  },
-
-  title: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-
   content: {
     maxHeight: 0,
     overflow: 'hidden',
   },
-
   contentShow: {
     height: 'auto',
     padding: 15,
     overflow: 'hidden',
     maxHeight: 9999,
   },
+  customerInfo: {
+    display: 'grid',
+    width: '90%',
+    gridTemplateColumns: '1fr 2fr',
+    gridTemplateRows: '1fr 2fr 1fr 1fr 2fr'
+  }
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,19 +4,19 @@ import {loadingReducer} from './loading/loading.reducers';
 import {loginReducer} from './login/login.reducers';
 import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import logger from 'redux-logger';
+import authReducer from './slices/authSlice';
 
 
-export const reducers = combineReducers({
-  loading: loadingReducer,
-  login: loginReducer,
-});
+// export const reducers = combineReducers({
+//   login: loginReducer,
+// });
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage
 };
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
